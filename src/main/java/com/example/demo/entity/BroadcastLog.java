1b9409cdebd9bff8dd0ee5df8c9176edafae6601
bala-amypo <bala@amypo.in>
2025-12-21 15:47:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/BroadcastLog.java
+++ src/main/java/com/example/demo/entity/BroadcastLog.java
@@ -1,18 +1,28 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.sql.Timestamp;
 
-
+@Getter
+@Setter
 @Entity
 public class BroadcastLog {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "event_update_id")
     private EventUpdate eventUpdate;
 
     @ManyToOne
+    @JoinColumn(name = "subscriber_id")
     private User subscriber;
 
     private String deliveryStatus;
+
     private Timestamp sentAt = new Timestamp(System.currentTimeMillis());
 }
--- src/main/java/com/example/demo/entity/EventUpdate.java
+++ src/main/java/com/example/demo/entity/EventUpdate.java
@@ -1,17 +1,25 @@
+package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.sql.Timestamp;
 
-
+@Getter
+@Setter
 @Entity
 public class EventUpdate {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "event_id")
     private Event event;
 
     private String updateContent;
+
     private String updateType;
 
     private Timestamp postedAt = new Timestamp(System.currentTimeMillis());
