0395ea1b5f2c1b7aa9af195cd977dada68364d94
bala-amypo <bala@amypo.in>
2025-12-21 15:44:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/Subscription.java
+++ src/main/java/com/example/demo/entity/Subscription.java
@@ -1,16 +1,25 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.sql.Timestamp;
 
-
+@Getter
+@Setter
 @Entity
 public class Subscription {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name = "event_id")
     private Event event;
 
     private Timestamp subscribedAt = new Timestamp(System.currentTimeMillis());
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,19 +1,27 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.sql.Timestamp;
 
-
+@Getter
+@Setter
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String fullName;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String password;
+
     private String role;
 
     private Timestamp createdAt = new Timestamp(System.currentTimeMillis());
