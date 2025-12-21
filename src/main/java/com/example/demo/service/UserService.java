765eb9b33292cb08befa6c3f5bffc2cef93523ff
bala-amypo <bala@amypo.in>
2025-12-21 15:14:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,17 @@
+@Service
+public class UserService {
+    private final UserRepository repo;
+    private final PasswordEncoder encoder;
+
+    public UserService(UserRepository repo, PasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    public User register(User u) {
+        if (repo.existsByEmail(u.getEmail()))
+            throw new IllegalArgumentException("Email already exists");
+        u.setPassword(encoder.encode(u.getPassword()));
+        return repo.save(u);
+    }
+}
