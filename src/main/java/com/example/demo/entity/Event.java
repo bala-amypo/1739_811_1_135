24aa93031ec305a663b79f87809df2663a9576f7
bala-amypo <bala@amypo.in>
2025-12-21 15:50:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -1,20 +1,31 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.sql.Timestamp;
 
-
-
+@Getter
+@Setter
 @Entity
 public class Event {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
+
     private String description;
+
     private String location;
+
     private String category;
+
     private boolean isActive = true;
 
     @ManyToOne
+    @JoinColumn(name = "publisher_id")
     private User publisher;
 
     private Timestamp createdAt = new Timestamp(System.currentTimeMillis());
