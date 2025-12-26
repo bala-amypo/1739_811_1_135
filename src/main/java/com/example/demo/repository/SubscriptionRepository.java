292dfd553c71115bbf11386d0ac49271e6bf659a
bala-amypo <bala@amypo.in>
2025-12-26 09:01:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/SubscriptionRepository.java
+++ src/main/java/com/example/demo/repository/SubscriptionRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    List<Subscription> findByUserId(Long userId);
 }
